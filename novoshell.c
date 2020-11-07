5263f4d6320321c3f820d53f876394ffa6a78b76
vhikehara <74071605+vhikehara@users.noreply.github.com>
2020-11-06 21:57:07 -0300

    Add files via upload

--- novoshell.c
+++ novoshell.c
@@ -20,21 +20,30 @@ int main(void){
 		fgets(entrada, TAMANHO, stdin);
 		
 		//ignorar input vazio
-		if(*p == '\n')continue;
-		
+		if(*p == '\n'){
+			continue;
+		}
 		//Convertendo o input para uma lista de argumentos
 		for(int i=0;i<sizeof(args)&&*p;p++){
-			if(*p == ' ') continue;
-			if(*p == '\n') break;
-			for(args[i++]= p; *p && *p != ' ' && *p != '\n'; p++);
-			*p ='\0';
+			if(*p == ' '){ 
+				continue;
+			}
+			if(*p == '\n'){
+				break;
+			}
+			for(args[i++]= p; *p && *p != ' ' && *p != '\n'; p++){
+				*p ='\0';
+			}
 		}
-		//built-in: exit
-		if(strcmp(SAIDA, args[0])==0) return 0;
-		
+		//se o usuário digitar quit então o programa fecha
+		if(strcmp(SAIDA, args[0])==0){
+			return 0;
+		}
 		//fork child and execute program
 		signal(SIGINT, SIG_DFL);
-		if(fork()==0) exit(execvp(args[0],args));
+		if(fork()==0){
+			exit(execvp(args[0],args));
+		}
 		signal(SIGINT, SIG_IGN);
 		
 		wait(&estado);
